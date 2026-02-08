23a59ef64360a0257c0c81bd692a66b20c732991
RizkiRamaDev <ramadhanrizki.dev@gmail.com>
2026-02-08 14:11:07 +0800

    Update main.js
    js toggle button dark mode

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log("Website Rizki aktif");
+const toggleButton = document.getElementById("theme-toggle");
+
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    toggleButton.textContent = "☀️ Light Mode";
+  } else {
+    toggleButton.textContent = "🌙 Dark Mode";
+  }
+});
